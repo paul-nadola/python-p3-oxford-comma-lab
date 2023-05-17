174f7dfc25f9a5ff3a0d5ef01b551586913e0299
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-05-17 14:19:44 +0300

    Done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
+
 def oxford_comma(items):
-    return None
+    if len(items) <= 2:
+        return ' and '.join(items)
+    else:
+        last_item = items[-1]
+        remaining_items = ', '.join(items[:-1])
+        return f"{remaining_items}, and {last_item}"
+
+print(oxford_comma(["fiddleheads", "okra", "disk", "not a disk", "kohlrabi"]))
